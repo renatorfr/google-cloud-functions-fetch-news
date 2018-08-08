924c0bb3ee07eef657053a97691ae31fca16a53e
Renato Freire Ricardo <rricardo@mirego.com>
2018-08-08 16:27:11 -0400

    Trying request

--- index.js
+++ index.js
@@ -6,8 +6,12 @@
  */
 const request = require('request');
  
-exports.request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  const message = body.url + ' ||| ' + body.explanation;
-  res.status(200).send(message);
-});
+exports.fetchNews = (req, res) => {
+  request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, response, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    const message = body.url + ' ||| ' + body.explanation;
+    res.status(200).send(message);
+  });
+};
